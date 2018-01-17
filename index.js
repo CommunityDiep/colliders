6c222f64a6817f077e688339dc5dc5957ad5eea7
haykam821 <haykam821@users.noreply.github.com>
2018-01-17 18:52:10 -0500

    Intersect method

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
-class Circle {};
-class Polygon {};
-class Vector {};
-class Line {};
+class Circle {
+	intersects(other) {
+		if (other instanceof Circle) {
+
+		} else if (other instanceof Polygon) {
+
+		} else if (other instanceof Vector) {
+
+		} else if (other instanceof Line) {
+
+		} else {
+			throw new TypeError("Cannot find intersection with an external (and thus unsupported) object.")
+		}
+	}
+};
+class Polygon {
+	intersects(other) {
+		if (other instanceof Circle) {
+
+		} else if (other instanceof Polygon) {
+
+		} else if (other instanceof Vector) {
+
+		} else if (other instanceof Line) {
+
+		} else {
+			throw new TypeError("Cannot find intersection with an external (and thus unsupported) object.")
+		}
+	}
+};
+class Vector {
+	intersects(other) {
+		if (other instanceof Circle) {
+
+		} else if (other instanceof Polygon) {
+
+		} else if (other instanceof Vector) {
+
+		} else if (other instanceof Line) {
+
+		} else {
+			throw new TypeError("Cannot find intersection with an external (and thus unsupported) object.")
+		}
+	}
+};
+class Line {
+	intersects(other) {
+		if (other instanceof Circle) {
+
+		} else if (other instanceof Polygon) {
+
+		} else if (other instanceof Vector) {
+
+		} else if (other instanceof Line) {
+
+		} else {
+			throw new TypeError("Cannot find intersection with an external (and thus unsupported) object.")
+		}
+	}
+};
 
 exports.Circle = Circle;
 exports.Polygon = Polygon;
