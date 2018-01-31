0dec096e4eb198e57e665abad8b088d2285a4305
CX <cx8128@gmail.com>
2018-01-30 22:34:39 -0500

    circle & vector

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 class Circle {
-	constructor() {
+	constructor(x, y, radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
 	}
-	
 	intersects(other) {
 		if (other instanceof Circle) {
-
+			let x = this.x - other.x;
+			let y = this.y - other.y;
+			return x * x + y * y < this.radius + other.radius;
 		} else if (other instanceof Polygon) {
 
 		} else if (other instanceof Vector) {
-
+			let x = this.x - other.x;
+			let y = this.y - other.y;
+			return x * x + y * y < this.radius;
 		} else if (other instanceof Line) {
 
 		} else {
@@ -35,16 +41,20 @@ class Polygon {
 	}
 };
 class Vector {
-	constructor() {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
 	}
 	
 	intersects(other) {
 		if (other instanceof Circle) {
-
+			let x = this.x - other.x;
+			let y = this.y - other.y;
+			return x * x + y * y < other.radius;
 		} else if (other instanceof Polygon) {
 
 		} else if (other instanceof Vector) {
-
+      return this.x === other.x && this.y === other.y;
 		} else if (other instanceof Line) {
 
 		} else {
